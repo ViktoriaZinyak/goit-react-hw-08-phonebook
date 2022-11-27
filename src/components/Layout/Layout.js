5d8f36be00190b536871489a18cce775acd1efd4
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-11-27 22:48:37 +0200

    no styled

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { AppBar } from './AppBar/AppBar';
+import { AppBar } from '../AppBar/AppBar';
 import { Suspense } from 'react';
+import { Wrap } from './Layout.styled';
 
 export const Layout = () => {
   return (
-    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    <Wrap>
       <AppBar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
       <Toaster position="top-right" reverseOrder={false} />
-    </div>
+    </Wrap>
   );
 };
---
+++ src/components/Layout/Layout.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Wrap = styled.div`
+  max-width: 960;
+  margin: '0 auto';
+  padding: '0 16px';
+`;
